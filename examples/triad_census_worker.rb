263b0c0dde20fa5239547f940ee908ebd0bfe913
Jeremy Blackburn <jeremy.blackburn@gmail.com>
2013-10-30 15:00:53 -0400

    allow passing the redis host

--- examples/triad_census_worker.rb
+++ examples/triad_census_worker.rb
@@ -8,9 +8,11 @@ node_id = ARGV[0]
 port = ARGV[1].to_i
 coordinator_node = ARGV[2]
 
+redis_host = ARGV[3] || "slavenode20.cse.usf.edu"
 
 
-DCell.start id: node_id, addr: "tcp://127.0.0.1:#{port}"
+
+DCell.start id: node_id, addr: "tcp://127.0.0.1:#{port}", registry: {adapter: 'redis', host: redis_host}
 
 Elaine::Distributed::Worker.supervise_as :worker, coordinator_node: coordinator_node
 sleep
