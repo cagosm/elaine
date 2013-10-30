111006c3913a2e811651f0a28df093603f111847
Jeremy Blackburn <jeremy.blackburn@gmail.com>
2013-10-30 15:16:41 -0400

    allow setting the host ip

--- examples/triad_census_coordinator.rb
+++ examples/triad_census_coordinator.rb
@@ -6,7 +6,9 @@ require 'dcell'
 puts "__FILE__: #{__FILE__}"
 load File.expand_path("./triad_census_vertex.rb")
 
-DCell.start id: "triad.census.elaine.coordinator", addr: "tcp://127.0.0.1:8100"
+host_ip = ARGV[0] || "127.0.0.1"
+
+DCell.start id: "triad.census.elaine.coordinator", addr: "tcp://#{host_ip}:8100"
 
 Elaine::Distributed::Coordinator.supervise_as :coordinator
 sleep
--- examples/triad_census_worker.rb
+++ examples/triad_census_worker.rb
@@ -1,4 +1,4 @@
-# bundle exec ruby triad_census_worker.rb triad.census.elaine.worker1 8101 triad.census.elaine.coordinator
+# bundle exec ruby triad_census_worker.rb triad.census.elaine.worker1 192.168.0.21 8101 triad.census.elaine.coordinator
 
 
 
@@ -10,14 +10,14 @@ require 'dcell'
 load File.expand_path("./triad_census_vertex.rb")
 
 node_id = ARGV[0]
-port = ARGV[1].to_i
-coordinator_node = ARGV[2]
+node_ip = ARGV[1] || "127.0.0.1"
+port = ARGV[2].to_i
+coordinator_node = ARGV[3]
+redis_host = ARGV[4] || "slavenode20.cse.usf.edu"
 
-redis_host = ARGV[3] || "slavenode20.cse.usf.edu"
 
 
-
-DCell.start id: node_id, addr: "tcp://127.0.0.1:#{port}", registry: {adapter: 'redis', host: redis_host}
+DCell.start id: node_id, addr: "tcp://#{host_ip}:#{port}", registry: {adapter: 'redis', host: redis_host}
 
 Elaine::Distributed::Worker.supervise_as :worker, coordinator_node: coordinator_node
 sleep
