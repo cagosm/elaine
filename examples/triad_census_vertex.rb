b21bd3230b81e897a47c2367596aa47d4cdb62f5
Jeremy Blackburn <jeremy.blackburn@gmail.com>
2013-11-04 12:13:38 -0500

    i think that type 1s might be working...

--- examples/triad_census_vertex.rb
+++ examples/triad_census_vertex.rb
@@ -82,8 +82,8 @@ class TriadCensusVertex < Elaine::Distributed::Vertex
           # type2s = (@outedges | msg[:neighborhood])
           # @value[:type2] += type2s.size - 2
 
-          @value[:type1_local] -= (@outedges | msg[:neighborhood]).size - 2
-          @value[:type1_test] += @value[:n] - ((@outedges | msg[:neighborhood]).size - 2)
+          # @value[:type1_local] -= (@outedges | msg[:neighborhood]).size - 2
+          @value[:type1] += @value[:n] - ((@outedges | msg[:neighborhood]).size)
 
 
 
