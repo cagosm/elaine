8e2acb24207e3c747a331a08d473859c39dd811b
Jeremy Blackburn <jeremy.blackburn@gmail.com>
2013-11-05 07:49:57 -0500

    removed commentd out code

--- lib/elaine/distributed/worker.rb
+++ lib/elaine/distributed/worker.rb
@@ -8,11 +8,6 @@ module Elaine
 
       attr_reader :vertices, :active, :vertices2
 
-      # def initialize(graph = [], id: "elaine-worker", addr: "tcp://127.0.0.1:8096")
-      #   raise 'empty worker graph' if graph.empty?
-      #   @vertices = graph
-      #   @active   = graph.size
-      # end
 
       def initialize(coordinator_node: "elaine.coordinator", g: [], zipcodes: {})
 
@@ -20,12 +15,8 @@ module Elaine
         @coordinator_node = coordinator_node
         DCell::Node[@coordinator_node][:coordinator].register_worker DCell.me.id
 
-        # Elaine::Distributed::PostOffice.supervise_as :postoffice
-        # this could/should probably be done in the constructor
-        # Celluloid::Actor[:postoffice].zipcodes = zipcodes
         @vertices = []
         @superstep_num = 0
-        # self.init_graph(g)
       end
 
       def init_graph(g=[])
@@ -42,6 +33,9 @@ module Elaine
         # *describing* the graph.
         # @vertices = graph
         # @active   = graph.size
+
+        # HACK the local vertices should be dealt with differently than
+        # the @vertices2 member
         @vertices2 = []
         g.each do |n|
           # n[:klazz].supervise_as n[:id], n[:id], n[:value], Celluloid::Actor[:postoffice], n[:outedges]
@@ -57,14 +51,6 @@ module Elaine
 
       # HACK this should be handled better...
       def init_superstep
-        # need to delivery all messages here first, to avoid race conditions
-        # @vertices.each do |v|
-        #   vertex = Celluloid::Actor[v]
-        #   # v.messages = PostOffice.instance.read(v.id)
-        #   vertex.messages = Celluloid::Actor[:postoffice].read_all(vertex.id)
-        #   vertex.active! if vertex.messages.size > 0
-        # end
-
         @vertices2.each do |v|
           v.messages = Celluloid::Actor[:postoffice].read_all(v.id)
         end
@@ -77,37 +63,14 @@ module Elaine
       end
 
       def superstep
-        # Thread.new do
-        # @superstep_num += 1
-        # debug "Running superstep #{@superstep_num}"
-        # @vertices.each do |v|
-        #   vertex = Celluloid::Actor[v]
-        #   # v.messages = PostOffice.instance.read(v.id)
-        #   vertex.messages = Celluloid::Actor[:postoffice].read_all(vertex.id)
-        #   vertex.active! if vertex.messages.size > 0
-        # end
-
-        # active = @vertices.select {|v| Celluloid::Actor[v].active?}
         active = @vertices2.select {|v| v.active?}
 
-        # futures = active.map { |v| Celluloid::Actor[v].future(:step) }
-        # futures.map { |f| f.value }
-        # active.each {|v| v.step}
+        
         pmap(active) do |v|
           v.step
         end
-
-
-        # @active = active.select {|v| Celluloid::Actor[v].active?}.size
-        @active = active.select {|v| v.active?}.size
-        
+        @active = active.select {|v| v.active?}.size    
       end
-
-      # def each_vertex_value(&block)
-      #   @vertices2.each do |v|
-      #     yield v.value.class
-      #   end
-      # end
 
       def vertex_values
         @vertices2.map { |v| {id: v.id, value: v.value} }
